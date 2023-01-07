43d5f3efce6034d9c481c28a1b85adfe56db675f
MaximSeveryn <severyn.maxim@gmail.com>
2023-01-07 18:51:40 +0200

    New

--- kanban-server/validators/board.validator.js
+++
@@ -1,9 +0,0 @@
-const { throwError, sendError, isStrFalsy } = require('../helper');
-
-const BOARD_NAME_EMPTY_ERR = throwError('Board name cannot be empty', 400);
-
-exports.boardNameValidator = (req, res, next) => {
-	isStrFalsy(req?.body?.board_name)
-		? sendError(res, BOARD_NAME_EMPTY_ERR)
-		: next();
-};
