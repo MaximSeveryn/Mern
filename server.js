88c82b00035410e28b3bffe9400800f5b803c891
MaximSeveryn <severyn.maxim@gmail.com>
2023-01-08 17:35:21 +0200

    header

--- server.js
+++ server.js
@@ -19,6 +19,26 @@ dotenv.config({ path: './env' });
 connectDB();
 
 const app = express();
+
+
+//new
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+//new
+
+
+
+
 app.use(express.json());
 
 if (process.env.MODE === 'development') {
