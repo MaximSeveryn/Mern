bc6200020745fe0b6381578ff9b675a2350a72e8
MaximSeveryn <severyn.maxim@gmail.com>
2023-01-08 17:40:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 
 //new
-const bcrypt = require('bcrypt')
+//const bcrypt = require('bcrypt')
 /*const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
