c44ea84842ed058a6186c1bc67d8f9c40ac375a7
MaximSeveryn <severyn.maxim@gmail.com>
2023-01-08 12:34:32 +0200

    F

--- server/utils/lib.js
+++
@@ -1,9 +0,0 @@
-export const dateToString = (date) => {
-  return date.toISOString().replace(/T.*$/, '');
-}
-
-export const addDays = (date, addedDays) => {
-  let newDate = new Date(date.getTime())
-  return new Date(newDate.setDate(newDate.getDate() + addedDays));
-}
-
